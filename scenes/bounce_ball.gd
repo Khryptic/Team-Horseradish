f0dc3b35f6aad0aad77fe8442daa93b604867d0e
Edward Numrich <emnumrich@gmail.com>
2025-02-04 23:05:29 -0500

    Update bounce_ball.gd

--- scenes/bounce_ball.gd
+++ scenes/bounce_ball.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export_category("Ball Properties")
 @export var speed: float = 100.0
 @export var downVel: float = 100.0
-@export var velReduction: float = 0.9
+@export var velRedMult: float = 0.9
 @export var gravityConstant: float = 15.0
 
 func _ready():
@@ -14,7 +14,7 @@ func _physics_process(delta: float) -> void:
 	# Bounce ball if it collides with anything, losing a little momentum
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		velocity = velocity.bounce(collision.get_normal()) * velReduction
+		velocity = velocity.bounce(collision.get_normal()) * velRedMult
 
 	# Add constant gravity
 	velocity += Vector2(0, gravityConstant)
