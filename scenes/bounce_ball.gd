e136ddcd6243e46e327d785eeed518eb3db65014
Edward Numrich <emnumrich@gmail.com>
2025-02-04 23:03:57 -0500

    Added variables for the ball that can be changed in the editor

--- scenes/bounce_ball.gd
+++ scenes/bounce_ball.gd
@@ -1,16 +1,20 @@
 extends CharacterBody2D
 
-var speed = 100.0
+@export_category("Ball Properties")
+@export var speed: float = 100.0
+@export var downVel: float = 100.0
+@export var velReduction: float = 0.9
+@export var gravityConstant: float = 15.0
 
 func _ready():
-	velocity = Vector2(0, 100).normalized() * speed
+	velocity = Vector2(0, downVel).normalized() * speed
 
 func _physics_process(delta: float) -> void:
 	
 	# Bounce ball if it collides with anything, losing a little momentum
 	var collision = move_and_collide(velocity * delta)
 	if collision:
-		velocity = velocity.bounce(collision.get_normal()) * 0.9
+		velocity = velocity.bounce(collision.get_normal()) * velReduction
 
 	# Add constant gravity
-	velocity += Vector2(0, 45)
+	velocity += Vector2(0, gravityConstant)
