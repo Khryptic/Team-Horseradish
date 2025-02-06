ff29cd29768a4e966794ceae8cf7c79c23507925
Edward Numrich <emnumrich@gmail.com>
2025-02-06 14:25:19 -0500

    Ball will respawn when it leaves screen

--- addons/softbody2d/softbody2d_rigidbody.gd
+++ addons/softbody2d/softbody2d_rigidbody.gd
@@ -4,4 +4,7 @@ extends RigidBody2D
 signal rigidbody_created(collision_shape: CollisionShape2D, is_center: bool)
 
 func _ready() -> void:
-	freeze = true
+	setFreeze(true)
+
+func setFreeze(isFrozen:bool) -> void:
+	set_deferred("freeze", isFrozen)
--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -4,28 +4,29 @@ extends Node2D
 
 var ballStart: Vector2
 var ball_ref
+var new_ball
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	ball_ref = get_tree().get_first_node_in_group("ball")
+	ball_ref = get_tree().get_first_node_in_group("ball").get_child(0)
+	ball_ref.add_to_group("ball")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
 func _respawn() -> void:
-	var new_ball = ball_prefab.instantiate()
-	new_ball.position = $"Respawn Anchor".global_position
-	new_ball.add_to_group("ball")
-	add_child(new_ball)
-	
-	ball_ref = get_tree().get_first_node_in_group("ball")
-
-func _on_bottom_bounds_body_entered(body: Node2D) -> void:
-	if body.is_in_group("ball"):
-		body.queue_free()
-		_respawn()
-
+	new_ball = ball_prefab.instantiate()
+	new_ball.position = $Respawn.global_position
+	new_ball.get_child(0).add_to_group("ball")
+	call_deferred("add_child", new_ball)
+		
+	ball_ref = new_ball.get_child(0)
 
 func _on_drawing_controller_trampoline_drawn() -> void:
-	ball_ref.freeze = false
+	ball_ref.setFreeze(false)
+
+func _on_bounds_body_entered(body: Node2D) -> void:
+	if body.is_in_group("ball"):
+		body.call_deferred("free")
+		_respawn()
