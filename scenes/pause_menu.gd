bfc072bc890049bc7ec1e38579e29c7881041b4b
Andrew-Ebersole <112182469+Andrew-Ebersole@users.noreply.github.com>
2025-02-18 16:09:34 -0500

    frees mouse while game paused

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -1,14 +1,18 @@
 extends Panel
 
 func _on_pause_button_pressed():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) # lets mouse exit window
+
 	get_tree().paused = true
 	show()
 
 func _on_resume_button_pressed():
-	
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED) # confines mouse to window
+
 	hide()
 	get_tree().paused = false
 
 func _on_restart_button_pressed():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED) # confines mouse to window
 	get_tree().paused = false
 	get_tree().reload_current_scene()
