28819e93b93ac3d3675df40755852801d6fa3608
TacoCat <ijk9988@rit.edu>
2025-02-20 13:49:18 -0500

    Add max fall speed to the ball

--- scenes/rigid_body_2d.gd
+++ scenes/rigid_body_2d.gd
@@ -2,8 +2,14 @@ extends RigidBody2D
 
 signal rigidbody_created(collision_shape: CollisionShape2D, is_center: bool)
 
+@export var maxFallSpeed: float
+
 func _ready() -> void:
 	setFreeze(true)
 
 func setFreeze(isFrozen: bool) -> void:
 	set_deferred("freeze", isFrozen)
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if(state.linear_velocity.y >= maxFallSpeed):
+		state.linear_velocity.y = maxFallSpeed
