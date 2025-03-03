c628a7965c0ebbe5623c5f27568f960b39ac6422
Andrew-Ebersole <112182469+Andrew-Ebersole@users.noreply.github.com>
2025-03-03 11:15:49 -0500

    Anti soft lock

--- scenes/bounce_ball.gd
+++ scenes/bounce_ball.gd
@@ -4,3 +4,13 @@ extends Node2D
 
 func freezeBall(isFrozen: bool) -> void:
 	rigidbody.setFreeze(isFrozen)
+
+func _process(delta: float) -> void:
+	if is_ball_stopped():
+		GameManager.clear_on_pegs()
+
+func is_ball_stopped() -> bool:
+	if is_instance_valid(rigidbody):
+		return rigidbody.linear_velocity.length() < 0.1	
+	return false
+		
