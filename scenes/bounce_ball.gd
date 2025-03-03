9c2b707850b31df3b60f2934f867f7b113061465
TacoCat <ijk9988@rit.edu>
2025-03-03 14:22:17 -0500

    Add star particles on crit

--- scenes/bounce_ball.gd
+++ scenes/bounce_ball.gd
@@ -3,12 +3,18 @@ class_name Ball extends Node2D
 @export var maxFallSpeed: float
 
 @onready var rigidbody: RigidBody2D = $RigidBody2D
+@onready var star_particles: GPUParticles2D = $StarParticles
 
 func _ready() -> void:
 	setFreeze(true)
 
 func setFreeze(isFrozen: bool) -> void:
 	rigidbody.set_deferred("freeze", isFrozen)
+
+func crit() -> void:
+	star_particles.position = rigidbody.position
+	star_particles.rotation = rigidbody.linear_velocity.angle() + PI/2
+	star_particles.restart()
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	if(state.linear_velocity.y >= maxFallSpeed):
