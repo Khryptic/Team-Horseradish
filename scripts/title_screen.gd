56ec56eb4ccd39cb073bce32fdac73ad3241a20c
Amanda <89926961+Sparrowe26@users.noreply.github.com>
2025-03-27 18:19:56 -0400

    added title screen, some new assets, & buttons

---
+++ scripts/title_screen.gd
@@ -0,0 +1,8 @@
+extends Button
+
+func _on_pressed():
+	$"../../AnimationPlayer".play("button_press")
+
+
+func _on_animation_player_animation_finished(button_press):
+	get_tree().change_scene_to_file("uid://c6usj5s4g5bdm")
