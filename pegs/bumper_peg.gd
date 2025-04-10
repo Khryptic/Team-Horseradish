3622b747cd314f3f903943e9c9caf9e590f3cc83
TacoCat <ijk9988@rit.edu>
2025-04-10 13:03:33 -0400

    Bumper peg works

---
+++ pegs/bumper_peg.gd
@@ -0,0 +1,9 @@
+extends Peg
+
+func _on_peg_hit(_body: RigidBody2D):
+    
+    # Call the parent method to handle the peg hit logic
+    super._on_peg_hit(_body)
+
+    var push_dir: Vector2 = (_body.position - position).normalized()
+    _body.apply_central_impulse(push_dir * 2000)
--- pegs/peg.gd
+++ pegs/peg.gd
@@ -35,7 +35,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 
-func _on_peg_hit():
+func _on_peg_hit(_body: RigidBody2D):
 
 	sprite2D.set_texture(peg_sprite.hit_sprite)
 
@@ -87,4 +87,3 @@ func increase_size():
 	$AnimationScale/Sprite2D.scale *= peg_scaler
 	$CollisionShape2D.scale *= peg_scaler
 	$peg_sensor/CollisionShape2D.scale *= peg_scaler
-	
