5d87b65869ef67adbf9e27959a8b883c93cbcfcb
TacoCat <ijk9988@rit.edu>
2025-04-14 11:33:01 -0400

    Changed bumper peg texture

--- pegs/bumper_peg.gd
+++ pegs/bumper_peg.gd
@@ -6,4 +6,4 @@ func _on_peg_hit(_body: RigidBody2D):
     super._on_peg_hit(_body)
 
     var push_dir: Vector2 = (_body.position - position).normalized()
-    _body.apply_central_impulse(push_dir * 2000)
+    _body.apply_central_impulse(push_dir * 1000)
