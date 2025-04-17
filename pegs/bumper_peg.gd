af11570c070dfecdd49dd52411a356bbce6c40d8
TacoCat <ijk9988@rit.edu>
2025-04-17 11:04:34 -0400

    Make bumper peg slightly larger

--- pegs/bumper_peg.gd
+++ pegs/bumper_peg.gd
@@ -1,9 +1,11 @@
 extends Peg
 
+@export var bumper_strength: int = 500
+
 func _on_peg_hit(_body: RigidBody2D):
     
     # Call the parent method to handle the peg hit logic
     super._on_peg_hit(_body)
 
     var push_dir: Vector2 = (_body.position - position).normalized()
-    _body.apply_central_impulse(push_dir * 1000)
+    _body.apply_central_impulse(push_dir * bumper_strength)
