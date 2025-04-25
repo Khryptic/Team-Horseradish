0c2fab10fe190cf6c56f9120e03ef3b4bc127306
TacoCat <ijk9988@rit.edu>
2025-04-24 22:30:12 -0400

    Chained peg works properly now

--- pegs/chained_peg.gd
+++ pegs/chained_peg.gd
@@ -2,23 +2,25 @@ extends Peg
 
 @export var chained_sprite: PegSprite
 
+@onready var sprite2D_overlay: Sprite2D = $AnimationScale/OverlaySprite2D
+
 var is_chained: bool = true
 
 func _ready():
 	
 	super._ready()
 
-	sprite2D.set_texture(chained_sprite.sprite)
+	sprite2D_overlay.set_texture(chained_sprite.sprite)
 	circle_shader.set_instance_shader_parameter('color', chained_sprite.color)
 
 func _on_peg_hit(_body: RigidBody2D):
 
-	# If the peg is not chained, treat it like a normal peg
+	# If the peg is not chained, just act like a normal peg
 	if(!is_chained):
 		super._on_peg_hit(_body)
 		
 	else:
-		sprite2D.set_texture(chained_sprite.hit_sprite)
+		sprite2D_overlay.set_texture(chained_sprite.hit_sprite)
 
 		animation_player.stop()
 		animation_player.play("ball_hit")
@@ -41,11 +43,14 @@ func _remove_peg():
 			is_chained = false
 			is_light_on = true
 
-			sprite2D.set_texture(peg_sprite.sprite)
+			sprite2D_overlay.visible = false
 			circle_shader.set_instance_shader_parameter('color', peg_sprite.color)
 
-			peg_sensor.points_worth = 30
+			peg_sensor.points_worth = 10
 
+			for i in range(randi_range(min_orb_count, max_orb_count)):
+				spawn_score_orb()
+			
 			AudioManager.create_audio(SoundEffect.SOUND_EFFECT_TYPE.PEG_DESTROY)
 
 			# Play the peg destroy animation
@@ -54,7 +59,7 @@ func _remove_peg():
 			particles.texture = chained_sprite.burst_sprite
 			particles.global_position = global_position
 
-	# Not chained, so call the parent method
+	# Not chained, so just act like a normal peg
 	else:
 		super._remove_peg()
 
